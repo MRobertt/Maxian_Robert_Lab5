57a86348f2c1266b17bdf49862f93a9f39bb97f2
eXpert <eXpert@DESKTOP-VFUUIRB>
2020-11-05 19:51:57 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Maxian_Robert_Lab5.Models;
+
+namespace Maxian_Robert_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+
+
+    }
+}
